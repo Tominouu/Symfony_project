f14e3e49d10206d827ce297f41606777bdc01428
Tom Leclercq <tomleclercq2006@gmail.com>
2025-03-03 15:18:51 +0100

    add project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController {
+final class HomeController extends AbstractController {
 
-    function index (): Response {
-        return new Response('Hello World');
+    #[Route("/", name: "home")]
+    function index (Request $request): Response {
+        return new Response('Bonjour ' . $request->get('name'));
     }
 }
+
